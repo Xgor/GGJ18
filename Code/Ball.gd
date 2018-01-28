7ca6b36d36640c2a3605afe431daefd424e085ae
Unknown <real.xgor@hotmail.com>
2018-01-28 17:01:07 +0100

    Final Jam version

--- Code/Ball.gd
+++ Code/Ball.gd
@@ -3,15 +3,16 @@ extends RigidBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-#onready var TweenNode = get_node("Tween")
+onready var TweenNode = get_node("Tween")
 var trail
+var s
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initial
 	#add_force(Vector2(0,0), Vector2(0,1)*400)
 	trail =load( "res://Objects/trail.tscn")
-#	TweenNode.interpolate_property(self,scale,1,0.0,0,Tween.TRANS_LINEAR,Tween.EASE_IN,0)
-#	TweenNode.start()
+	TweenNode.interpolate_property(self, "scale", Vector2(1, 1), Vector2(0.0, 0.0), 20,Tween.TRANS_LINEAR,Tween.EASE_IN)
+	TweenNode.start()
 	pass
 
 func fire(rot,firePower):
@@ -25,7 +26,7 @@ func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
 	
-	
+	s = scale
 	if position.y > 1000 or linear_velocity.length_squared() < 1:
 		get_tree().call_group("GameManager", "stop")
 	pass
@@ -39,7 +40,6 @@ func _on_Ball_body_entered( body ):
 		if group == "Bumper":
 			var angle = position-body.position
 			angle =angle.normalized()
-			print(angle)
 			linear_velocity = angle*500
 			#set_axis_velocity(angle*10)
 	
@@ -53,7 +53,14 @@ func _on_Ball_body_shape_entered( body_id, body, body_shape, local_shape ):
 
 func _on_Timer_timeout():
 	var o = trail.instance()
-	get_parent().call_deferred("add_child",o)
 	o.position = position
-	o.scale = scale
+	print(s)
+#	o.scale = Vector2(0.5,0.5)
+	get_parent().call_deferred("add_child",o)
+	#print(scale)
+	pass # replace with function body
+
+
+func _on_Tween_tween_completed( object, key ):
+	TweenNode.stop()
 	pass # replace with function body
